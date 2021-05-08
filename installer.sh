36334e5ac5bbcbba34621815ae06f30d9fb4c99e
Saatwick <saatwick.chandra@acquia.com>
2021-05-08 19:33:10 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 cd $(find ~ -name "covid-vaccine-notifier-browser" 2> /dev/null)
 echo "Installing virtualenv"
-pip install virtualenv
+pip3 install virtualenv
 echo "Creating your virtual environment"
 virtualenv venv_browser
 echo "Activating your virtual environment"
 source venv_browser/bin/activate
 echo "Installing python modules"
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 echo "Setup done!"
